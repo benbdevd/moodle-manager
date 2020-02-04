604c36c6431486ec5c4abca26e513a830814cd57
Benjamin J. Boyle <me@benboyle.ca>
2020-02-04 13:34:05 -0400

    many changes, see desc.
    - Split up functions
    - Reworked functions to use globals
    - Working functions for scraping course ids
    - Added init function to handle setup
    - Added some interactivity to driver
    - Added logic to check/cleanup server address from user
    - Added skeleton for json file creation
    - Commented out code that is erroneous and will be changed soon

--- moodle_manager.py
+++ moodle_manager.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup as BS
+from getpass import getpass
 import requests
 import json
 import re
@@ -51,43 +52,61 @@ def s_print(s, style):
 
 
 def login(username, password):
-    session = requests.Session()
     payload = {
-        'logintoken': get_session_id(session),
+        'logintoken': get_session_id(),
         'username': username,
         'password': password
     }
-    session.post('https://moodle31.upei.ca/login/index.php', data=payload)
-    return session
+    session.post(server_url + LOGIN_URLPART, data=payload)
 
 
-def get_session_id(session):
-    soup = BS(session.get(
-        'https://moodle31.upei.ca/login/index.php').text, 'html.parser')
+def get_session_id():
+    soup = BS(session.get(server_url + LOGIN_URLPART).text, 'html.parser')
     return soup.find('input', attrs={'name': 'logintoken'})['value']
 
 
-def get_current_courses(session):
-    current_courses = []
-    r = session.get(baseurl + 'my/?myoverviewtab=courses')
-
-    if(r.status_code == 200):
-        soup = BS(r.text, 'html.parser')
-        current_courses = soup.find(id='pc-for-in-progress')
-
-    return current_courses
+def get_course_ids(get_all):
+    if get_all:
+        return get_all_course_ids
+    else:
+        return get_current_course_ids
 
 
-def get_all_courses(session):
-    all_courses = []
-    r = session.get(baseurl + 'my/?myoverviewtab=courses')
+def get_all_course_ids():
+    soup = get_my_courses_soup()
 
-    if(r.status_code == 200):
-        soup = BS(r.text, 'html.parser')
-        all_courses = soup.find_all(
-            'div', class_='card mb-3 courses-view-course-item')
+    all_course_elements = soup.find_all(
+        'div', class_='card mb-3 courses-view-course-item')
 
-    return all_courses
+    return get_course_ids_from_elements(all_course_elements)
+
+
+def get_current_course_ids():
+    current_course_elements = []
+
+    soup = get_my_courses_soup()
+    temp = soup.find(id='pc-for-in-progress')
+
+    current_course_elements = [
+        element for element in temp.contents[1].contents if element != '\n' and element != ' ']
+
+    return get_course_ids_from_elements(current_course_elements)
+
+
+def get_my_courses_soup():
+    request = session.get(server_url + MY_COURSES_URLPART)
+    if(request.status_code == 200):
+        return BS(request.text, 'html.parser')
+    else:
+        return -1
+
+
+def get_course_ids_from_elements(elements):
+    course_ids = []
+    for element in elements:
+        course_ids.append(element.contents[1].attrs['href'].split('=')[1])
+
+    return course_ids
 
 
 def get_moodle_doc(url):
@@ -106,9 +125,8 @@ def write_doc(doc, name):
     file.close
 
 
-def download_std_moodle():
-    website = session.get('https://moodle31.upei.ca/course/view.php?id='
-                          + str(config['course_id']))
+def download_std_moodle(course_id):
+    website = session.get(server_url + COURSE_VIEW_URLPART + course_id)
     html = website.text
     soup = BS(html, 'html.parser')
     links = soup.findAll(class_='activityinstance')
@@ -118,54 +136,45 @@ def download_std_moodle():
 
     for link in links:
         name = link.find(class_='instancename').text
-        if config['match'] not in name.lower() or name in config['downloaded']:
-            continue
+        # if config['match'] not in name.lower() or name in config['downloaded']:
+        #     continue
         print(name)
         url = link.a['href']
         doc = get_moodle_doc(url)
         write_doc(doc['doc'], doc['name'])
-        config['downloaded'].append(name)
+        # config['downloaded'].append(name)
         print('\t', doc['name'])
 
 
 def download_cezar():
-    semester = config['semester'].split('-')
-    year = semester[0]
-    season = semester[1]
-    course = config['course']
-    website = requests.get(
-        'http://www.smcs.upei.ca/~ccampeanu/Teach/'
-        + season
-        + '/'
-        + year
-        + '/'
-        + course
-        + '/LN/'
-    )
-    html = website.text
-    links = re.findall('"(http://.*4.pdf?)"', html)
+    # semester = config['semester'].split('-')
+    # year = semester[0]
+    # season = semester[1]
+    # course = config['course']
+    # website = requests.get(
+    #     'http://www.smcs.upei.ca/~ccampeanu/Teach/'
+    #     + season
+    #     + '/'
+    #     + year
+    #     + '/'
+    #     + course
+    #     + '/LN/'
+    # )
+    # html = website.text
+    # links = re.findall('"(http://.*4.pdf?)"', html)
 
     print('Downloading:')
     print('===========')
 
-    for link in links:
-        name = link[(link.rfind('/')+1):]
-        print(name)
-        doc = session.get(link).content
-        write_doc(doc, name)
+    # for link in links:
+    #     name = link[(link.rfind('/')+1):]
+    #     print(name)
+    #     doc = session.get(link).content
+    #     write_doc(doc, name)
 
 
-session = login()
-print(get_current_courses(session))
-
-if __name__ == '__main__':
+def json_file():
     config = json.loads(open('config.json', 'r').read())
-    session = login()
-
-    if config['course_type'] == 'std':
-        download_std_moodle()
-    elif config['course_type'] == 'cezar':
-        download_cezar()
 
     config_file = open('config.json', 'w')
     pretty_json = json.dumps(config,
@@ -174,3 +183,36 @@ if __name__ == '__main__':
                              )
     config_file.write(pretty_json)
     config_file.close()
+
+
+def init(server_url_param, username, password):
+    global session, server_url
+    session = requests.Session()
+    server_url = server_url_param
+    login(username, password)
+
+
+def validate_server_url():
+    global server_url
+    server_url = server_url.replace('http://', 'https://')
+
+    if not server_url.startswith('https://'):
+        server_url = 'https://' + server_url
+
+    if not server_url.endswith('/'):
+        server_url = server_url + '/'
+
+
+# DRIVER
+if __name__ == '__main__':
+    interactive = True
+
+    if interactive:
+        s_print(splash, Style.WARNING)
+        server_url = input('Moodle Server: ')
+        username = input('Moodle Username: ')
+        password = getpass('Moodle Password: ')
+
+    init(server_url, username, password)
+    print(get_all_course_ids())
+    print(get_current_course_ids())
